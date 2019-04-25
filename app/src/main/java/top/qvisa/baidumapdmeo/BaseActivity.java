b56adf69b9bb9c81c15520de9ea064529cbe6246
qvistary <qvistary@gmail.com>
2019-04-25 23:17:32 +0800

    修改动态权限的提示逻辑与显示

--- app/src/main/java/top/qvisa/baidumapdmeo/BaseActivity.java
+++ app/src/main/java/top/qvisa/baidumapdmeo/BaseActivity.java
@@ -1,22 +1,24 @@
 package top.qvisa.baidumapdmeo;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Settings;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseActivity extends AppCompatActivity {
 
-    public void request_Permission(String[] permissions) {
+    private View layoutRoot;
+
+    public void request_Permission(String[] permissions, View view) {
+        this.layoutRoot = view;
         List<String> permissionList = new ArrayList<>();
         for (int i = 0; i < permissions.length; i++) {
             if (ContextCompat.checkSelfPermission(BaseActivity.this, permissions[i])
@@ -31,36 +33,43 @@ public class BaseActivity extends AppCompatActivity {
 
     }
 
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, final String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case 1: {
                 if (grantResults.length > 0) {
                     for (int result : grantResults)
                         if (result != PackageManager.PERMISSION_GRANTED) {
-                            new AlertDialog
-                                    .Builder(this)
-                                    .setCancelable(false)
-                                    .setTitle("错误信息：")
-                                    .setMessage("需要授权全部的访问权限，否则将无法使用！")
-                                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            Intent intent = new Intent();
-                                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                            //设置去向意图
-                                            Uri uri = Uri.fromParts("package", BaseActivity.this.getPackageName(), null);
-                                            intent.setData(uri);
-                                            //发起跳转
-                                            startActivity(intent);
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(BaseActivity.this,
+                                    permissions[0])) {
+                                Snackbar.make(layoutRoot, "你需要同意软件请求的权限！", Snackbar.LENGTH_SHORT).setAction(
+                                        "去请求", new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                request_Permission(permissions, layoutRoot);
+                                            }
                                         }
-                                    })
-                                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            finish();
+                                ).show();
+                            } else {
+                                Snackbar.make(layoutRoot, "你需要同意软件请求的权限！", Snackbar.LENGTH_SHORT).setAction(
+                                        "去设置", new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Intent intent = new Intent();
+                                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                //设置去向意图
+                                                Uri uri = Uri.fromParts("package", BaseActivity.this.getPackageName(),
+                                                        null);
+                                                intent.setData(uri);
+                                                //发起跳转
+                                                startActivity(intent);
+                                            }
                                         }
-                                    }).show();
+                                ).show();
+
+                            }
+
+
                             break;
                         }
                 }
--- app/src/main/java/top/qvisa/baidumapdmeo/MainActivity.java
+++ app/src/main/java/top/qvisa/baidumapdmeo/MainActivity.java
@@ -1,21 +1,22 @@
 package top.qvisa.baidumapdmeo;
 
-
 import android.Manifest;
 import android.os.Bundle;
-
+import android.support.design.widget.CoordinatorLayout;
 
 public class MainActivity extends BaseActivity {
-    private  String[] permissions={
-            Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.ACCESS_FINE_LOCATION
+    private String[] permissions = {
+            //  Manifest.permission.READ_PHONE_STATE,//电话
+            // Manifest.permission.WRITE_EXTERNAL_STORAGE,//存储
+            Manifest.permission.ACCESS_FINE_LOCATION//定位权限
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        request_Permission(permissions);
+        CoordinatorLayout coordinatorLayout = findViewById(R.id.layout_coordinator);
+        request_Permission(permissions, coordinatorLayout);
+
     }
 }
